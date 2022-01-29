825cf034dd9d90ea6f3a7ff8dfd87a22e5e2290d
sashaFPV <andrei.cazanov2017@gmail.com>
2022-01-29 17:02:47 +0200

    Chart: работает на дефолтных значениях

---
+++ Crypto/wwwroot/js/chart/chart.js
@@ -0,0 +1,19 @@
+let myChart = document.getElementById("chart").getContext("2d");
+
+let massPopChart = new Chart(myChart, {
+    type: "line",
+    backgroundColor: 'rgb(42,48,66)',
+    data: {
+        labels: ['Boston', 'Work', 'Help', 'Me', 'Pls'],
+        datasets: [
+            {
+                label: 'Currency',
+                data: [100, 200, 300, 400, 200],
+                borderColor: '#dea74b',
+                backgroundColor: 'rgba(94, 105, 44, 0.15)',
+                borderWidth: 1,
+                fill: true
+            }
+        ],
+    },
+});

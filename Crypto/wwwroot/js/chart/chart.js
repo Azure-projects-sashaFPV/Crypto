df6c31633abb254b76495dfd4c3e2689787715cb
sashaFPV <andrei.cazanov2017@gmail.com>
2022-01-31 23:41:39 +0200

    Chart: текущее изменение цены на 80%

--- Crypto/wwwroot/js/chart/chart.js
+++ Crypto/wwwroot/js/chart/chart.js
@@ -1,5 +1,6 @@
 let myChart = document.getElementById("chart").getContext("2d");
 
+
 let massPopChart = new Chart(myChart, {
     type: "line",
     backgroundColor: 'rgb(42,48,66)',
@@ -18,9 +19,9 @@ let massPopChart = new Chart(myChart, {
     }
 });
 
+//document.querySelectorAll('.current_price');
 const getCurrentPrices = (symbol) => {
     var burl = `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1d&limit=2`;
-    let mass = [];
     var url = burl;
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', url, true);
@@ -30,63 +31,22 @@ const getCurrentPrices = (symbol) => {
 
     ourRequest.onload = function () {
         response = JSON.parse(ourRequest.responseText);
-        let dates = [];
         let prices = [];
 
 
         for (let item of response) {
-            let date = new Date(item[0]);
-            let resultDate = formatDate(date);
-            mass[count++] = new Currency(resultDate, item[4]);
+            prices[count++] = item[4];
         }
-
-        let i = 0;
-        for (let item of mass) {
-            dates[i] = item.date;
-            prices[i] = item.price;
-            i++;
+        let res = prices[1] - prices[0];
+        let price = prices[1];
+        if (res >= 0) {
+            document.getElementById(`${symbol}`).className = "price increase my-invisible";
+            document.getElementById(`${symbol}`).childNodes[1].textContent = Number(price).toFixed(2) + " $";
         }
-        //console.log(dates);
-        //console.log(`Name : ${name}`);
-        //console.log(`Response : ${response}`);
-        // -------------------------------------------------------------
-        massPopChart.destroy(); // !!!
-        massPopChart = new Chart(myChart, {
-            type: "line",
-            backgroundColor: 'rgb(42,48,66)',
-            data: {
-                labels: dates,
-                datasets: [
-                    {
-                        label: name,
-                        data: prices,
-                        borderColor: '#dea74b',
-                        backgroundColor: 'rgba(94, 105, 44, 0.15)',
-                        borderWidth: 1,
-                        fill: true
-                    }
-                ],
-            },
-            options: {
-                plugins: {
-                    tooltip: {
-                        callbacks: {
-                            label: function (context) {
-                                var label = context.dataset.label || '';
-
-                                if (label) {
-                                    label += ': ';
-                                }
-                                if (context.parsed.y !== null) {
-                                    label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
-                                }
-                                return label;
-                            }
-                        }
-                    }
-                }
-            }
-        });
+        else {
+            document.getElementById(`${symbol}`).className = "price decrease my-invisible";
+            document.getElementById(`${symbol}`).childNodes[1].textContent = Number(price).toFixed(2) + " $";
+        }
     }
     ourRequest.send();
 }
@@ -174,12 +134,11 @@ class Currency {
 }
 
 $('.crypto-item').click((event) => {
-    console.log(event);
     let abbreviation = event.target.firstElementChild.lastElementChild.lastElementChild.outerText;
     let crypto_name = event.target.firstElementChild.lastElementChild.textContent;
     getChart(abbreviation, crypto_name);
+    getCurrentPrices(abbreviation);
 });
-
 const formatDate = (date) => {
     let dateOfyear = date.getFullYear() + ""; // год;
     let newDateOfyear = dateOfyear.slice(2); // год последние две цифры;
